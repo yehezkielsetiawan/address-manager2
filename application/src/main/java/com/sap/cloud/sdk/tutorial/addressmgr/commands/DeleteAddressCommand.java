98763f25e60c16caa68aa690194a618ea60296f0
Yehezkiel Setiawan <Yehezkiel.Setiawan@soltius.co.id>
2019-11-19 11:11:49 +0700

    address-manager2

---
+++ application/src/main/java/com/sap/cloud/sdk/tutorial/addressmgr/commands/DeleteAddressCommand.java
@@ -0,0 +1,46 @@
+package com.sap.cloud.sdk.tutorial.addressmgr.commands;
+
+import com.sap.cloud.sdk.odatav2.connectivity.ODataDeleteResult;
+import com.sap.cloud.sdk.s4hana.connectivity.ErpHttpDestination;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartnerAddress;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.services.DefaultBusinessPartnerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeleteAddressCommand {
+    private static final Logger logger = LoggerFactory.getLogger(DeleteAddressCommand.class);
+
+    private final BusinessPartnerService businessPartnerService;
+    private final String businessPartnerId;
+    private final String addressId;
+    private final ErpHttpDestination destination;
+
+    public DeleteAddressCommand(ErpHttpDestination destination,
+                                String businessPartnerId, String addressId) {
+
+        this(destination, new DefaultBusinessPartnerService(), businessPartnerId, addressId);
+    }
+
+
+    public DeleteAddressCommand(ErpHttpDestination destination, BusinessPartnerService businessPartnerService,
+                                String businessPartnerId, String addressId) {
+        this.destination = destination;
+        this.businessPartnerService = businessPartnerService;
+        this.businessPartnerId = businessPartnerId;
+        this.addressId = addressId;
+    }
+
+    public Integer execute() throws Exception {
+        // replace with code lines from Exercise 05
+        //return null;
+        final BusinessPartnerAddress addressToDelete = BusinessPartnerAddress.builder()
+                .businessPartner(businessPartnerId)
+                .addressID(addressId)
+                .build();
+        final ODataDeleteResult oDataDeleteResult = businessPartnerService
+                .deleteBusinessPartnerAddress(addressToDelete)
+                .execute(destination);
+        return oDataDeleteResult.getHttpStatusCode();
+    }
+}
---
+++ application/src/main/java/com/sap/cloud/sdk/tutorial/addressmgr/commands/GetSingleBusinessPartnerByIdCommand.java
@@ -0,0 +1,53 @@
+package com.sap.cloud.sdk.tutorial.addressmgr.commands;
+
+import com.sap.cloud.sdk.s4hana.connectivity.ErpHttpDestination;
+import com.sap.cloud.sdk.s4hana.connectivity.ErpHttpDestinationUtils;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartnerAddress;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GetSingleBusinessPartnerByIdCommand {
+
+    private static final Logger logger = LoggerFactory.getLogger(GetSingleBusinessPartnerByIdCommand.class);
+    private static final String CATEGORY_PERSON = "1";
+
+    private final BusinessPartnerService businessPartnerService;
+
+    private final String id;
+
+    private static final String DESTINATION_NAME = "S4HC_SYSTEM";
+
+    final ErpHttpDestination destination = ErpHttpDestinationUtils
+            .getErpHttpDestination(DESTINATION_NAME);
+
+
+    public GetSingleBusinessPartnerByIdCommand(final BusinessPartnerService service, final String id) {
+        businessPartnerService = service;
+        this.id = id;
+    }
+
+
+    public BusinessPartner execute() throws Exception {
+        // replace with code lines from Exercise 05
+        //return null;
+        return businessPartnerService.getBusinessPartnerByKey(id)
+                .select(BusinessPartner.BUSINESS_PARTNER,
+                        BusinessPartner.LAST_NAME,
+                        BusinessPartner.FIRST_NAME,
+                        BusinessPartner.IS_MALE,
+                        BusinessPartner.IS_FEMALE,
+                        BusinessPartner.CREATION_DATE,
+                        BusinessPartner.TO_BUSINESS_PARTNER_ADDRESS.select(
+                                BusinessPartnerAddress.BUSINESS_PARTNER,
+                                BusinessPartnerAddress.ADDRESS_ID,
+                                BusinessPartnerAddress.COUNTRY,
+                                BusinessPartnerAddress.POSTAL_CODE,
+                                BusinessPartnerAddress.CITY_NAME,
+                                BusinessPartnerAddress.STREET_NAME,
+                                BusinessPartnerAddress.HOUSE_NUMBER))
+                .execute(destination);
+
+    }
+}
